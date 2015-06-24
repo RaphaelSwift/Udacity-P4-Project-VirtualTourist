aacfd008e0fd22fc9db773fc0dbb845c431bb889
Raphael Neuenschwander <neuenschwander.raphael@gmail.com>
2015-06-24 17:53:18 +0200

    Added FetMethod Flickr

---
+++ FlickrClient-Constants.swift
@@ -0,0 +1,9 @@
+//
+//  FlickrClient-Constants.swift
+//  VirtualTourist
+//
+//  Created by Raphael Neuenschwander on 24.06.15.
+//  Copyright (c) 2015 Raphael Neuenschwander. All rights reserved.
+//
+
+import Foundation
